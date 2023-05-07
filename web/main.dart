1df6b3724a648310249efe18fd05314d655b3091
nuramani393 <nuramani393@gmail.com>
2023-05-08 00:38:28 +0800

    saved

--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,7 @@ import 'dart:html';
 
 void main() {
   // Initialize list with 22 empty slots
-  List<int> hoursWorked = List.filled(22, 0);
+  // List<int> hoursWorked = List.filled(22, 0);
 
   // Get HTML elements
   DateInputElement startDate = querySelector('#start-date') as DateInputElement;
@@ -11,16 +11,16 @@ void main() {
   ButtonElement saveButton =
       querySelector('input[type="submit"]') as ButtonElement;
 
-  TableElement workingHours = querySelector('#employeeTable') as TableElement;
+  // TableElement workingHours = querySelector('#employeeTable') as TableElement;
 
   // Add event listener for calculate pay button
   saveButton.onClick.listen((MouseEvent event) {
-    event.preventDefault(); // prevent form from submitting
+    // event.preventDefault(); // prevent form from submitting
 
     // Get input values
-    String date = startDate.value!;
-    String start = startTime.value!;
-    String end = endTime.value!;
+    String? date = startDate.value;
+    String? start = startTime.value;
+    String? end = endTime.value;
 
     // // Save the input values to localStorage
     // window.localStorage['date'] = date;
@@ -28,9 +28,9 @@ void main() {
     // window.localStorage['end'] = end;
 
     // Calculate hours worked
-    int startHour = int.parse(start.split(':')[0]);
+    int startHour = int.parse(start!.split(':')[0]);
     int startMinute = int.parse(start.split(':')[1]);
-    int endHour = int.parse(end.split(':')[0]);
+    int endHour = int.parse(end!.split(':')[0]);
     int endMinute = int.parse(end.split(':')[1]);
 
     int minutesWorked =
@@ -46,10 +46,10 @@ void main() {
     // Calculate payable amount and EPF contribution
     double payableAmount = hoursWorking * 6;
     // double epfContribution = payableAmount * 0.11;
-
     var displayElement = querySelector('#output');
     displayElement?.text = "Payable Amount: RM$payableAmount";
 
+    print(displayElement);
     // // Add row to employee table
     // TableRowElement row = workingHours.tBodies.first!.insertRow(-1);
     // TableCellElement dateCell = row.insertCell(-1)..text = date;
