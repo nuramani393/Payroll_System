69ee6bd53164768de3f05996c578dab6df48c735
nuramani393 <nuramani393@gmail.com>
2023-05-08 14:12:57 +0800

    working output

--- web/main.dart
+++ web/main.dart
@@ -4,37 +4,57 @@ void main() {
   // Initialize list with 22 empty slots
   // List<int> hoursWorked = List.filled(22, 0);
 
-  // Get HTML elements
-  DateInputElement startDate = querySelector('#start-date') as DateInputElement;
-  TimeInputElement startTime = querySelector('#start-time') as TimeInputElement;
-  TimeInputElement endTime = querySelector('#end-time') as TimeInputElement;
-  ButtonElement saveButton = querySelector('#save') as ButtonElement;
+  // Get the form element and submit button
+  var form = querySelector('form');
+  form!.onSubmit.listen((event) {
+    // Prevent the form from submitting
+    event.preventDefault();
 
-  // TableElement workingHours = querySelector('#employeeTable') as TableElement;
+    // Get the input values
+    var dateValue = (querySelector('#start-date') as InputElement).value;
+    var startTimeValue = (querySelector('#start-time') as InputElement).value;
+    var endTimeValue = (querySelector('#end-time') as InputElement).value;
 
-  // Add event listener for calculate pay button
-  saveButton.onClick.listen((MouseEvent event) {
-    // event.preventDefault(); // prevent form from submitting
+    // // Create a data object with the input values
+    // Map<String, String> data = {
+    //   'date': dateValue!,
+    //   'start-time': startTimeValue!,
+    //   'end-time': endTimeValue!
+    // };
 
-    // Get input values
-    String? date = startDate.value;
-    String? start = startTime.value;
-    String? end = endTime.value;
+    // Get the table body element
+    var table = querySelector('#WorkingHours') as TableSectionElement;
+    var newRow = table.insertRow(-1); // -1 means "insert at the end"
+    newRow.insertCell(0).text = dateValue;
+    newRow.insertCell(1).text = startTimeValue;
+    newRow.insertCell(2).text = endTimeValue;
+  });
+}
+    
 
-    // // Save the input values to localStorage
-    // window.localStorage['date'] = date;
-    // window.localStorage['start'] = start;
-    // window.localStorage['end'] = end;
+// Function to calculate hours worked from start time and end time
+// double calculateHoursWorked(String startTime, String endTime) {
+//   var start = DateTime.parse("2023-01-01 " + startTime);
+//   var end = DateTime.parse("2023-01-01 " + endTime);
+//   var duration = end.difference(start);
+//   return duration.inMinutes / 60.0;
+// }
+
+// Function to calculate payable amount from hours worked
+// double calculatePayableAmount(double hoursWorked) {
+//   return hoursWorked * 10.0; // Assuming $10 per hour
+// }
+
 
     // Calculate hours worked
-    int startHour = int.parse(start!.split(':')[0]);
-    int startMinute = int.parse(start.split(':')[1]);
-    int endHour = int.parse(end!.split(':')[0]);
-    int endMinute = int.parse(end.split(':')[1]);
+    // int startHour = int.parse(start!.split(':')[0]);
+    // int startMinute = int.parse(start.split(':')[1]);
+    // int endHour = int.parse(end!.split(':')[0]);
+    // int endMinute = int.parse(end.split(':')[1]);
 
-    int minutesWorked =
-        (endHour * 60 + endMinute) - (startHour * 60 + startMinute);
-    int hoursWorking = minutesWorked ~/ 60;
+    // int minutesWorked =
+    //     (endHour * 60 + endMinute) - (startHour * 60 + startMinute);
+    // int hoursWorking = minutesWorked ~/ 60;
 
     // // Update working hours list
     // int index = getEmptySlotIndex(hoursWorked);
@@ -43,12 +63,10 @@ void main() {
     // }
 
     // Calculate payable amount and EPF contribution
-    double payableAmount = hoursWorking * 6;
-    // double epfContribution = payableAmount * 0.11;
-    var displayElement = querySelector('#output');
-    displayElement?.text = "Payable Amount: RM$payableAmount";
+    // double payableAmount = hoursWorking * 6;
+    // // double epfContribution = payableAmount * 0.11;
 
-    print(displayElement);
+
     // // Add row to employee table
     // TableRowElement row = workingHours.tBodies.first!.insertRow(-1);
     // TableCellElement dateCell = row.insertCell(-1)..text = date;
@@ -58,15 +76,5 @@ void main() {
     //   ..text = 'RM ' + payableAmount.toStringAsFixed(2);
     // TableCellElement epfContributionCell = row.insertCell(-1)
     //   ..text = 'RM ' + epfContribution.toStringAsFixed(2);
-  });
-}
+  
 
-// Helper function to get index of first empty slot in working hours list
-int getEmptySlotIndex(List<int> workingHours) {
-  for (int i = 0; i < workingHours.length; i++) {
-    if (workingHours[i] == 0) {
-      return i;
-    }
-  }
-  return -1;
-}
